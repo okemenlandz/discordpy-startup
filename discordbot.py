3e0df75212631dd235c7aa3667fa8305fb5673e2
okemenlandz <85884961+okemenlandz@users.noreply.github.com>
2021-06-14 23:19:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -31,7 +31,33 @@ async def on_ready():
     await alert_channel.send(msg)
     await alert_channel.send(version)
     
+@bot.event
+async def on_voice_state_update(member, before, after): 
+	if member.guild.id == 744596357561712782 and (before.channel != after.channel):
+		alert_channel = bot.get_channel(744596357561712785)
+		if before.channel is None:
+			msg = f'{member.name}は{after.channel.name}'
+			await alert_channel.send(msg)
+		else:
+			msg = f'{member.name}は{before.channel.name}から{after.channel.name}に移動した！'
+			await alert_channel.send(msg)
+   
+	if member.guild.id == 741963356663185529 and (before.channel != after.channel):
+		alert_channel = bot.get_channel(741963356663185533)
+		if before.channel is None:
+			msg = f'{member.name}が{after.channel.name}で通話を開始した。'
+			await alert_channel.send(msg)
+		else:
+			msg = f'{member.name}は{before.channel.name}から{after.channel.name}に移動した！'
+			await alert_channel.send(msg)
+            
+@bot.command()
+async def ping(ctx):
+	await ctx.send('pong')
 
+@bot.command()
+async def pong(ctx):
+	await ctx.send('ping\npong')
         
            
 bot.run(token)

0b2b2a47e59602fb20e5c041656b7c939fdeca59
okemenlandz <85884961+okemenlandz@users.noreply.github.com>
2021-06-28 04:10:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -117,7 +117,7 @@ async def nmki(ctx):
 	nmki_list1 = ['実力','ラック']
 	nmki_list2 = ['あり','なし']
 	random.shuffle(nmki_list1)
-	ramdom.shuhhle(nmki_list2)
+	ramdom.shuffle(nmki_list2)
 	await ctx.send(nmki_list1[0] + nmki_list2[0] + nmki_list1[1] + nmki_list2[1])
 
 @bot.command()

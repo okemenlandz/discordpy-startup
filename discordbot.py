36aa77dc378e05a33d04b2cd8d8cd9cbb16595b8
kimura <akabane0223haru@gmail.com>
2024-04-06 01:08:55 +0900

    python ver

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,25 @@
 import discord
-from discord.ext import commands
-from discord.ext import tasks
-from datetime import datetime 
-import os
-import re
-import random
 import traceback
-import math
-import packages.mysql.connector as mydb
+from discord.ext import commands
+from os import getenv
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN'] #heroku
-version = 'ver 8.0'
+intents = discord.Intents.default()
+intents.message_content = True
 
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)

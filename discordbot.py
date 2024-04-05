a1d9e4a12a519363f59a667a482ccb642d8154fc
kimura <akabane0223haru@gmail.com>
2024-04-06 01:21:35 +0900

    pacxhi rebase

--- discordbot.py
+++ discordbot.py
@@ -178,6 +178,239 @@ async def gag(ctx):
 	else:
 		await ctx.send(f'あなたのギャグは{per}%です')
 
+@bot.command()
+async def gb(ctx,*args):
+	gap_list = [27,81,136,193,251,311,374,400]
+	err_flg = False
+	player_num = len(args)
+	players = []
+	gaps = [0,0,0,0]
+	temp = 0
+	message = ''
+
+	if len(args) == 0:
+		await ctx.send('正しい数字を入れてください')
+	elif len(args) > 4:
+		await ctx.send('パラメータが多すぎます')
+	else:
+		for i in range(player_num):
+			if not (args[i].isdecimal()):
+				err_flg = True
+		if err_flg:
+			await ctx.send('正しい数字を入れてください')
+		else:
+			for i in range(player_num):
+				players.append(int(args[i]))
+
+			for i in range(player_num):
+				gaps[i] = 0
+				for j in range(player_num):
+					if players[i] > players[j]:
+						temp = players[i] - players[j]
+						for k in range(8):
+							if temp <= gap_list[k]:
+								break
+							gaps[i] -= 1
+					elif players[i] < players[j]:
+						temp = players[j] - players[i]
+						for k in range(8):
+							if temp <= gap_list[k]:
+								break
+							gaps[i] += 1
+
+				if not i == 0:
+					message += "\n"
+				message += str(gaps[i])
+			await ctx.send(message)
+
+@bot.command()
+async def val(ctx,*args):
+	machine_list1 = ['とある','大海物語','アクエリオン','ウルトラセブン','エヴァ','仮面ライダー','ゴルゴ','牙狼','仕置人','無双','乙女','タイガーマスク']
+	machine_list2 = ['HOTD','まどか','モモキュン','ゆゆゆ','リングミドル','リングライトミドル','冬ソナ']
+	machine_list = machine_list1 + machine_list2
+	if len(args) == 0:
+		await ctx.send('登録されている機種はこちらです')
+		mc_disp = ''
+		for mc in machine_list:
+			mc_disp = mc_disp + '・' + mc + '\n'
+		await ctx.send(mc_disp)
+
+def right():
+	right = random.randint(0,36)
+	if right < 5:
+		pl_rand = random.randint(0,99)
+		if pl_rand < 40:
+			return 16
+		elif pl_rand < 43:
+			return 12
+		elif pl_rand < 50:
+			return 8
+		else:
+			return 4
+	return 0
+
+@bot.command()
+async def god(ctx):
+	flag = True
+	normal_cnt = 0
+	while flag:
+		v = random.randint(0,8191)
+		normal_cnt += 1
+		if v == 8191:
+			flag = False
+	await ctx.send(f'{normal_cnt}回転で当選しました。')
+
+
+@bot.command()
+async def symphogear(ctx):
+	flag = True
+	normal_cnt = 0
+	while flag:
+		v = random.randint(0,19980)
+		normal_cnt += 1
+		if v > 19879:
+			flag = False
+			
+	in_money = math.ceil(normal_cnt / 10.5) * 500
+	rest = math.ceil(((0 - (normal_cnt * 2)) % 21) * 125 / 21)
+	await ctx.send(f'{normal_cnt}回転で当選しました。')
+
+	pl = ''
+	judge = ''
+	
+	if v == 19980:
+		cnt = [0,0,0,0,1]
+		await ctx.send('全回転:tada:')
+	else:
+		cnt = [0,1,0,0,0]
+
+		for i in range(5):
+			r_ch = right()
+			if r_ch == 0:
+				cl = random.randint(0,9999)
+				if cl < 4910:
+					pl += ':blue_square:'
+				elif cl < 9420:
+					pl += ':green_square:'
+				elif cl < 9987:
+					pl += ':red_square:'
+				else:
+					pl += ':yellow_square:'
+				judge += ':x:'
+				cnt[0] += 1
+			elif r_ch == 16:
+				cl = random.randint(0,39)
+				if cl < 3:
+					pl += ':rainbow:'
+				else:
+					cl = random.randint(0,97)
+					if cl < 29:
+						pl += ':blue_square:'
+					elif cl < 58:
+						pl += ':green_square:'
+					elif cl < 82:
+						pl += ':red_square:'
+					else:
+						pl += ':yellow_square:'
+				judge += '(15)'
+				cnt[0] = 0
+				cnt[4] += 1
+			else:
+				cl = random.randint(0,97)
+				if cl < 29:
+					pl += ':blue_square:'
+				elif cl < 58:
+					pl += ':green_square:'
+				elif cl < 82:
+					pl += ':red_square:'
+				else:
+					pl += ':yellow_square:'
+				judge += f'({r_ch})'
+				cnt[0] = 0
+				cnt[int(r_ch/4)] += 1
+
+		await ctx.send(f'{pl}.')
+		await ctx.send(f'{judge}.')
+		judge = ''
+		
+	if cnt[0] == 5:
+		await ctx.send(f'最終決戦終了\n投資:{in_money}円\n回収:{1480 + rest * 4}円\n収支:{1480 + rest * 4 - in_money}円')
+	else:
+		await ctx.send('シンフォギアチャンス　突入')
+		while(cnt[0] < 11):
+			r_ch = right()
+			if r_ch == 0:
+				judge += ':x:'
+				cnt[0] += 1
+			elif r_ch == 16:
+				judge += '(15)'
+				await ctx.send(judge)
+				judge = ''
+				cnt[0] = 0
+				cnt[4] += 1
+			else:
+				judge += f'({r_ch})'
+				await ctx.send(judge)
+				judge = ''
+				cnt[0] = 0
+				cnt[int(r_ch/4)] += 1
+		await ctx.send(f'{judge}\nシンフォギアチャンス　終了\nFEVER×{cnt[1]+cnt[2]+cnt[3]+cnt[4]}\n(4)×{cnt[1]}\n(8)×{cnt[2]}\n(12)×{cnt[3]}\n(15)×{cnt[4]}')
+		total = (cnt[1]*370+cnt[2]*740+cnt[3]*1120+cnt[4]*1410+rest)*4
+		await ctx.send(f'投資:{in_money}円\n回収:{total}円\n収支:{total - in_money}円')
+
+def right_g():
+	right = random.randint(0,205)
+	if right < 80:
+		return 3
+	elif right < 100:
+		return 9
+	else:
+		return 0
+
+@bot.command()
+async def gen(ctx):
+	flag = True
+	normal_cnt = 0
+	while flag:
+		v = random.randint(0,65535)
+		normal_cnt += 1
+		if v > 65329:
+			flag = False
+			
+	in_money = math.ceil(normal_cnt / 10.5) * 500
+	rest = math.ceil(((0 - (normal_cnt * 2)) % 21) * 125 / 21)
+	await ctx.send(f'{normal_cnt}回転で当選しました。')
+
+	pl = ''
+	judge = ''
+	
+	if v == 65535:
+		cnt = [0,0,1,0]
+		await ctx.send('ロングフリーズ:tada:')
+	elif v > 65411:
+		cnt = [0,0,1,0]
+	else:
+		cnt = [0,0,0,0]
+		
+	if cnt[2] == 0:
+		await ctx.send(f'チャレンジ失敗\n投資:{in_money}円\n回収:{2640 + rest * 4}円\n収支:{2640 + rest * 4 - in_money}円')
+	else:
+		await ctx.send('超源RUSH 突入')
+		cnt = [0,1,0,0]
+		while(cnt[0] < 4):
+			r_ch = right_g()
+			if r_ch == 0:
+				judge += ':x:'
+				cnt[0] += 1
+			else:
+				judge += f'({r_ch})'
+				await ctx.send(judge)
+				judge = ''
+				cnt[0] = 0
+				cnt[int(r_ch/3)] += 1
+		await ctx.send(f'{judge}\n超源RUSH　終了\n超源RUSH×{cnt[1]+cnt[2]}\n超源BONUS×{cnt[3]}')
+		total = (cnt[1]*330+cnt[2]*660+cnt[3]*990+rest)*4
+		await ctx.send(f'投資:{in_money}円\n回収:{total}円\n収支:{total - in_money}円')
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)

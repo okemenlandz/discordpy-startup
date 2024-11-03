e8eaee1641926766c9a9b58a20f19a23414962e8
kimura <akabane0223haru@gmail.com>
2024-11-04 01:53:23 +0900

    db追加

--- discordbot.py
+++ discordbot.py
@@ -707,7 +707,8 @@ async def aria(ctx):
 async def regist(ctx,*args):
 	url = "https://okemenlandz.sakura.ne.jp/okemenlandz/public/api/moneys/"
 	res = requests.get(url + str(ctx.author.id))
-
+	name = ""
+	
 	if len(args) == 0:
 		name = ctx.author.global_name
 	else:
